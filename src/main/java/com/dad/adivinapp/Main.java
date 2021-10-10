cddc0b0003d24e0580d81bc2c93d090394a95aae
Darel M <cls18drl@gmail.com>
2021-10-10 10:53:38 +0100

    p

--- src/main/java/com/dad/adivinapp/Main.java
+++
@@ -1,7 +0,0 @@
-package com.dad.adivinapp;
-
-public class Main {
-    public static void main(String[] args) {
-        AdivinApp.main(args);
-    }
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.dad.adivinapp.adivinapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.dad.adivinapp to javafx.fxml;
-    exports com.dad.adivinapp;
-}
